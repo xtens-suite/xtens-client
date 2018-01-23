267e35ea94139f262a7e312e35e722e21a4365eb
zigur <massimorgon@gmail.com>
2018-01-23 17:51:58 +0000

    fixed wrong rel parsing

--- src/utils/funcs.js
+++ src/utils/funcs.js
@@ -21,7 +21,7 @@ export function parseLinkHeader(header) {
             throw new Error('section could not be split on \';\'');
         }
         const url = section[0].replace(/<(.*)>/, '$1').trim();
-        const name = section[1].replace(/rel="(.*)"/, '$1').trim();
+        const name = section[1].replace(/rel=(.*)/, '$1').trim();
         links[name] = url;
     }
     return links;
