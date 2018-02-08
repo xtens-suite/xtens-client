ae9920bf89e30c9f46687e4db2458f64a005738c
zigur <massimorgon@gmail.com>
2018-02-08 18:19:18 +0000

    completed pagination

--- src/components/subcomponents/Pagination.vue
+++ src/components/subcomponents/Pagination.vue
@@ -1,30 +1,31 @@
 <template>
     <ul class="pagination">
         <li class="page-item">
-            <a class="page-link" href="#" @click.prevent="onPageChange(0)" aria-label="First">
+            <a class="page-link" :class="{ disabled: isFirst }" href="#" @click.prevent="onPageChange(0)" aria-label="First">
                 <span aria-hidden="true">First</span>
             </a>
         </li>
         <li  class="page-item">
-            <a class="page-link" href="#" @click.prevent="onPageChange(currentPage-1)" aria-label="Previous">
+            <a class="page-link" :class="{ disabled: isFirst }" href="#" @click.prevent="onPageChange(currentPage-1)" aria-label="Previous">
                 <span aria-hidden="true">Previous</span>
             </a>
         </li>
         <li  class="page-item">
-            <a class="page-link" href="#" >
+            <a class="page-link disabled" href="#" >
                 <span aria-hidden="true">Page {{ currentPage + 1 }} of {{ totalPages }}</span>
             </a>
         </li>
         <li  class="page-item">
-            <a class="page-link" href="#" @click.prevent="onPageChange(currentPage+1)" aria-label="Next">
+            <a class="page-link" :class="{ disabled: isLast }" href="#" @click.prevent="onPageChange(currentPage+1)" aria-label="Next">
                 <span aria-hidden="true">Next</span>
             </a>
-        </li  class="page-item">
-            <a class="page-link" href="#" @click.prevent="onPageChange(totalPages-1)" aria-label="Last">
+        </li>
+        <li  class="page-item">
+            <a class="page-link" :class="{ disabled: isLast }" href="#" @click.prevent="onPageChange(totalPages-1)" aria-label="Last">
                 <span aria-hidden="true">Last</span>
             </a>
-    </li>
-</ul>
+        </li>
+    </ul>
 </template>
 
 <script>
@@ -54,6 +55,16 @@ export default {
         return {};
     },
 
+    computed: {
+        isFirst() {
+            return this.currentPage <= 0;
+        },
+
+        isLast() {
+            return this.currentPage >= this.totalPages - 1;
+        }
+    },
+
     methods: {
         onPageChange(pageIndex) {
             if (pageIndex < 0 || pageIndex >= this.totalPages) return;
@@ -70,3 +81,13 @@ export default {
 
 };
 </script>
+
+<style scoped lang="scss">
+$disabled-opacity: 0.65;
+a.disabled {
+    pointer-events: none;
+    opacity: $disabled-opacity;
+    box-shadow: none;
+    cursor: not-allowed;
+}
+</style>
--- test/unit/components/subcomponents/Pagination.spec.js
+++ test/unit/components/subcomponents/Pagination.spec.js
@@ -7,47 +7,109 @@ import Pagination from '@/components/subcomponents/Pagination';
 
 describe('Pagination.vue', function() {
 
-    describe('onPageChange', function() {
+    const paginationProps = {
+        recordType: 'subject',
+        projects: [],
+        activeProject: undefined,
+        currentPage: 4,
+        totalPages: 20,
+        itemsPerPage: 10,
+        totalItems: 196,
+        links: {
+            first: 'http://localhost:1337/subject?limit=10',
+            previous: 'http://localhost:1337/subject?limit=10&skip=20',
+            next: 'http://localhost:1337/subject?limit=10&skip=40',
+            last: 'http://localhost:1337/subject?limit=10&skip=190'
+        }
+    };
 
-        let stub, wrapper;
+    describe('methods', function() {
 
-        beforeEach(function() {
-            stub = sinon.stub(store, 'dispatch');
-            wrapper = shallow(Pagination, {
-                store,
-                propsData: {
-                    recordType: 'subject',
-                    projects: [],
-                    activeProject: undefined,
-                    currentPage: 4,
-                    totalPages: 20,
-                    itemsPerPage: 10,
-                    totalItems: 196,
-                    links: {
-                        first: 'http://localhost:1337/subject?limit=10',
-                        previous: 'http://localhost:1337/subject?limit=10&skip=20',
-                        next: 'http://localhost:1337/subject?limit=10&skip=40',
-                        last: 'http://localhost:1337/subject?limit=10&skip=190'
-                    }
-                }
+        describe('onPageChange', function() {
+
+            let stub, wrapper;
+
+            beforeEach(function() {
+                stub = sinon.stub(store, 'dispatch');
+                wrapper = shallow(Pagination, {
+                    store,
+                    propsData: paginationProps
+                });
             });
+
+            afterEach(function() {
+                stub.restore('records/getSubjects');
+            });
+
+            it('should trigger the dispatch method with correct action and pagination info as payload', function() {
+                wrapper.vm.onPageChange(6);
+                expect(stub.calledOnce).to.be.true;
+                expect(stub.calledWith()).to.be.true;
+            });
+
+            it('should ritorn without triggering dispatch if param is out of page bounds', function() {
+                wrapper.vm.onPageChange(-1);
+                expect(stub.called).to.be.false;
+                wrapper.vm.onPageChange(21);
+                expect(stub.called).to.be.false;
+            });
+
         });
 
-        afterEach(function() {
-            stub.restore('records/getSubjects');
+    });
+
+    describe('computed', function() {
+
+        describe('isFirst', function() {
+
+            let wrapper;
+
+            it('should return true if it\'s set on the first page', function() {
+                wrapper = shallow(Pagination, {
+                    store,
+                    propsData: {
+                        ...paginationProps,
+                        currentPage: 0,
+                        links: {} // let's omit the links here
+                    }
+                });
+                expect(wrapper.vm.isFirst).to.be.true;
+            });
+
+            it('should return false if it\'s not set on the first page', function() {
+                wrapper = shallow(Pagination, {
+                    store,
+                    propsData: paginationProps
+                });
+                expect(wrapper.vm.isFirst).to.be.false;
+            });
+
         });
 
-        it('should trigger the dispatch method with correct action and pagination info as payload', function() {
-            wrapper.vm.onPageChange(6);
-            expect(stub.calledOnce).to.be.true;
-            expect(stub.calledWith()).to.be.true;
-        });
+        describe('isLast', function() {
 
-        it('should ritorn without triggering dispatch if param is out of page bounds', function() {
-            wrapper.vm.onPageChange(-1);
-            expect(stub.called).to.be.false;
-            wrapper.vm.onPageChange(21);
-            expect(stub.called).to.be.false;
+            let wrapper;
+
+            it('should return true if it\'s set on the last page', function() {
+                wrapper = shallow(Pagination, {
+                    store,
+                    propsData: {
+                        ...paginationProps,
+                        currentPage: paginationProps.totalPages - 1,
+                        links: {} // let's omit the links here
+                    }
+                });
+                expect(wrapper.vm.isLast).to.be.true;
+            });
+
+            it('should return false if it\'s not set on the last page', function() {
+                wrapper = shallow(Pagination, {
+                    store,
+                    propsData: paginationProps
+                });
+                expect(wrapper.vm.isLast).to.be.false;
+            });
+
         });
 
     });
